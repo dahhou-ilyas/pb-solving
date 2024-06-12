fb450bb39ad2d32d08784ccc3307b61ecd92c35b
dahhou ilyas <110790236+dahhou-ilyas@users.noreply.github.com>
2024-06-12 03:22:10 +0100

    Time: 30 ms (71.83%), Space: 51.1 MB (82.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,8 +3,11 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         if(nums==null || nums.length < 3) return new ArrayList<>();
         Arrays.sort(nums);
-        Set<List<Integer>> resuLists=new HashSet<>();
+        List<List<Integer>> resuLists=new ArrayList<>();
         for (int i =0;i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
             int left=i+1;
             int right =nums.length-1;
 
@@ -15,6 +18,10 @@ class Solution {
                     resuLists.add(Arrays.asList(nums[i] , nums[left] ,nums [right]));
                     left++;
                     right--;
+                    while (nums[left]==nums[left-1] && left<right){
+                        left++;
+                    }
+                    
                 } else if (sum<0) {
                     left++;
                 }else {
@@ -22,6 +29,7 @@ class Solution {
                 }
             }
         }
-        return new ArrayList<>(resuLists);
+
+        return resuLists;
     }
 }
