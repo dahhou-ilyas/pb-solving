a9563a29ca464b0f1b45fa41e8abd8cfce90619b
dahhou ilyas <110790236+dahhou-ilyas@users.noreply.github.com>
2024-07-02 14:36:44 +0100

    Time: 43 ms (83.36%), Space: 111.2 MB (90.16%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,28 @@
+class LRUCache {
+    private final int capacity;
+    private final LinkedHashMap<Integer, Integer> cache;
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
