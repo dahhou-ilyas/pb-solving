e2d6d2dffe558b1b8d2981d985e8f132232d4aef
dahhou ilyas <110790236+dahhou-ilyas@users.noreply.github.com>
2024-07-02 14:46:15 +0100

    Time: 45 ms (68.95%), Space: 112.7 MB (72.79%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,24 +1,77 @@
 class LRUCache {
+   class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+        
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
     private final int capacity;
-    private final LinkedHashMap<Integer, Integer> cache;
+    private final Map<Integer, Node> cache;
+    private Node head;
+    private Node tail;
     
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
-            @Override
-            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-                return size() > capacity;
-            }
-        };
+        this.cache = new HashMap<>();
+        this.head = new Node(0, 0); // Dummy head node
+        this.tail = new Node(0, 0); // Dummy tail node
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
     }
+    
     public int get(int key) {
-        return cache.getOrDefault(key, -1);
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            // Move accessed node to the front
+            removeNode(node);
+            addNodeToHead(node);
+            return node.value;
+        }
+        return -1;
     }
     
     public void put(int key, int value) {
-        cache.put(key, value);
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            removeNode(node);
+            addNodeToHead(node);
+        } else {
+            if (cache.size() >= capacity) {
+                // Remove the least recently used node (tail.prev)
+                Node toRemove = tail.prev;
+                removeNode(toRemove);
+                cache.remove(toRemove.key);
+            }
+            // Add new node to the front
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addNodeToHead(newNode);
+        }
+    }
+    
+    private void addNodeToHead(Node node) {
+        Node next = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = next;
+        next.prev = node;
+    }
+    
+    private void removeNode(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
     }
 }
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
