dc2f6a0037e4b8a73167bb074ea19f9f398815b5
dahhou ilyas <110790236+dahhou-ilyas@users.noreply.github.com>
2024-07-04 19:16:00 +0100

    Time: 1 ms (99.78%), Space: 62.3 MB (7.60%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        int min=prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }else{
+                maxProfit=Math.max(maxProfit,prices[i]-min);
+            }
+            
+        }
+        return maxProfit;
+    }
+}
