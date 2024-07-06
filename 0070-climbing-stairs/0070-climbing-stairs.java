907a4b71fadfe867ded7374798e6931b91fbb661
dahhou ilyas <110790236+dahhou-ilyas@users.noreply.github.com>
2024-07-06 23:13:34 +0100

    Time: 0 ms (100.00%), Space: 40.5 MB (18.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; ++i){
-         dp[i] = dp[i - 1] + dp[i - 2];   
-        }
+        int prev1 = 1; // dp[i - 1]
+    int prev2 = 1; // dp[i - 2]
 
-        return dp[n];
+    for (int i = 2; i <= n; ++i) {
+      final int dp = prev1 + prev2;
+      prev2 = prev1;
+      prev1 = dp;
+    }
+
+    return prev1;
     }
 
 }
