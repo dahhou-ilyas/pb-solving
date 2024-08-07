88296d96be2e29ec2982af502f547ac2e08a8353
dahhou ilyas <110790236+dahhou-ilyas@users.noreply.github.com>
2024-08-07 03:16:52 +0100

    Time: 181 ms (5.48%), Space: 47.1 MB (5.56%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] candyv1=new int[ratings.length];
+        int[] candyv2 =new int[ratings.length];
+        candyv1[0]=1;
+        candyv2[candyv2.length-1]=1;
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i]>ratings[i-1]){
+                candyv1[i]=candyv1[i-1]+1;
+            }else{
+                candyv1[i]=1;
+            }
+        }
+        for(int i=ratings.length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                candyv2[i]=candyv2[(i+1)]+1;
+            }else{
+                candyv2[i]=1;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<ratings.length;i++){
+            System.out.println("candyv2 = "+candyv2[i]+" candyv1 = "+candyv1[i] );
+            sum=sum+Math.max(candyv2[i],candyv1[i]);
+        }
+        return sum;   
+    }
+}
